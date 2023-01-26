8f411fc806a2f0f9654d7b2e864c263a9ed1323c
Ismail Boukoti <109099866+IsmailBoukoti@users.noreply.github.com>
2023-01-26 14:25:02 +0100

    Fixed HeaderController mapping

--- src/main/java/co/develhope/firstapi4/controllers/HeaderController.java
+++ src/main/java/co/develhope/firstapi4/controllers/HeaderController.java
@@ -3,12 +3,14 @@ package co.develhope.firstapi4.controllers;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/headers")
 class HeaderController{
 
-    @GetMapping("/headers")
+    @GetMapping("/")
     public String getHeaders(@RequestHeader HttpHeaders headers){
         return headers.getHost().getHostName() + " " + headers.getHost().getPort();
     }
